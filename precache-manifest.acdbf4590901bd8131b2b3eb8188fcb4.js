26f008467f7a7e3a1dc729968f4eef62f2c60885
sujata <gunalesujata@gmail.com>
2020-08-18 18:49:41 +0530

    Updates

---
+++ precache-manifest.acdbf4590901bd8131b2b3eb8188fcb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0b110fba583735e50ef8f58e6038a6",
+    "url": "/Spotify-Premium/index.html"
+  },
+  {
+    "revision": "f8833338b2204930a72f",
+    "url": "/Spotify-Premium/static/css/2.1a8daee9.chunk.css"
+  },
+  {
+    "revision": "6561ceabd61bcd650417",
+    "url": "/Spotify-Premium/static/css/main.7e2910c6.chunk.css"
+  },
+  {
+    "revision": "f8833338b2204930a72f",
+    "url": "/Spotify-Premium/static/js/2.5d42232b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Spotify-Premium/static/js/2.5d42232b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6561ceabd61bcd650417",
+    "url": "/Spotify-Premium/static/js/main.c25712d0.chunk.js"
+  },
+  {
+    "revision": "12b14ac922e36b3e00e9",
+    "url": "/Spotify-Premium/static/js/runtime-main.43362841.js"
+  }
+]);
